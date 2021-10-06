51fe9a6daafbad8436cc8766800c732c72de982d
Bernitafebri <bernitafebrihatin@students.undip.ac.id>
2021-10-06 11:07:49 +0700

    drawer menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "antd/dist/antd.css";
+import { Drawer, Button } from 'antd';
+import {MenuOutlined } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+
+const [visible, setVisible] = useState(false);
+
+return (
+	<div style={{
+    display: 'block', width: 700, padding: 30, color:'black',
+    }}>
+    {/* <h4>ReactJS Ant-Design Drawer Component</h4> */}
+    
+      <>
+          <Button type="primary"
+          onClick={() => {
+            setVisible(true);
+          }}><MenuOutlined /></Button>
+
+            <Drawer
+            title="Drawer Demo"
+            placement="left"
+            closable={false}
+            visible={visible}
+            onClose={() => {
+              setVisible(false)
+            }}
+            >
+
+              <Button>
+                ite
+              </Button>
+            <p>Item One</p>
+            <p>Item Two</p>
+            <p>Item Three</p>
+            <p>Item Four</p>
+            <p>Item Five</p>
+            </Drawer>
+      </>
+  </div>
   );
 }
-
-export default App;
